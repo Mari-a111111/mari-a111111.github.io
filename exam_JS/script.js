baa7f14a1ca82a286d383a31646b884ea98ba902
Mari-a111111 <ndchfjfvm@gmail.com>
2025-12-19 10:39:18 +0200

    Add files via upload

--- exam_JS/script.js
+++ exam_JS/script.js
@@ -1,17 +1,23 @@
-const input = document.getElementById("myInput");
+const input = document.getElementById('myInput');
 
-document.onmouseup = function () {
-  setTimeout(() => {
-    const selection = window.getSelection();
-    const selectedText = selection.toString().trim();
+document.onmouseup = function() {
+    setTimeout(() => {
+        const selection = window.getSelection();
+        const selectedText = selection.toString().trim();
 
-    if (selectedText) {
-      input.value = selectedText;
-      selection.removeAllRanges();
-    }
-  }, 0);
+        if (selectedText) {
+            input.value = selectedText;
+            
+            selection.removeAllRanges();
+            
+            input.style.borderColor = '#2ecc71';
+            setTimeout(() => {
+                input.style.borderColor = '#4a6bbf';
+            }, 500);
+        }
+    }, 0);
 };
 
-input.onclick = function () {
-  this.select();
+input.onclick = function() {
+    this.select();
 };
